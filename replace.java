0270a6817a3afc87b6c6bd77b1eb5e3c8c2dcf37
Gowri sree Anilkumar <85624436+gowri30@users.noreply.github.com>
2022-01-26 11:41:52 +0400

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,10 @@
+import javax.swing.text.html.StyleSheet;
+
+public class replace {
+     public static void main(String args[]){
+        int n,i;
+        int a[]=new a[20];
+        Scanner obj=new Scanner(System.in);
+        
+     }
+}
